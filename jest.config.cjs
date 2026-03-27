1ac0ed216dd5f4c601be57c65125dd910b5c375b
vennyv <nneomav04@gmail.com>
2026-03-27 18:28:32 -0400

    clean up

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest'
-  },
-  moduleFileExtensions: ['js', 'jsx', 'json'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-};
