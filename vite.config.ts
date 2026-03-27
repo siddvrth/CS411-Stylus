e6bd363841efa8dcf80fa0cbc65ffc32f86cbeb8
vennyv <nneomav04@gmail.com>
2026-03-27 13:55:26 -0400

    Move frontend files into frontend/ and update config paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  root: path.resolve(__dirname, "frontend"),
   server: {
     host: "::",
     port: 8080,
@@ -14,7 +15,10 @@ export default defineConfig(({ mode }) => ({
   plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./frontend/src"),
     },
+  },
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
   },
 }));
